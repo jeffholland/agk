09bb6054efdf7f47e83601a9de0928e4635ed5af
Jeff Holland <holland.musictech@gmail.com>
2023-02-18 10:26:52 -0500

    moved all the effect work into the base class - subclasses now trivial. do we even need them anymore?

--- effect.py
+++ effect.py
@@ -5,20 +5,31 @@ from colors import colors
 # Base class for the Effect module
 
 class Effect(tk.Frame):
-    def __init__(self, master, name):
+    def __init__(self, master, name, params):
+
+        # params - we expect a list of dictionaries in the format:
+        # {
+        #   "name": "Amplify",
+        #   "min": 0.01,
+        #   "max": 0.99
+        # }
+        #
+        # this is all an Effect subclass needs to specify
+
         tk.Frame.__init__(self, master, bg=colors["hl1"])
 
         self.master = master
         self.name = name
+        self.params = params
 
-        # dimensions - height can be changed
+        # dimensions - height based on num params
         self.width = self.master.master.master.master.width
-        self.height = 80
+        self.height = 50 + (30 * len(self.params))
 
-        self.widgets = []
+        # empty arrays to store the GUI elements
+        self.labels = []
         self.entries = []
-
-        self.params = []
+        self.vars = []
 
         self.create_widgets()
         self.refresh_colors()
@@ -26,11 +37,11 @@ class Effect(tk.Frame):
     def create_widgets(self):
         self.configure(width=self.width, height=self.height)
 
-        numcolumns = 2
+        numcolumns = 4
         for i in range(numcolumns):
             self.columnconfigure(i, minsize=self.width / numcolumns)
 
-        # labels
+        # name
 
         self.name_label = tk.Label(
             self,
@@ -39,27 +50,64 @@ class Effect(tk.Frame):
         self.name_label.grid(
             row=0, 
             column=0,
-            columnspan=2
+            columnspan=numcolumns
         )
-        self.widgets.append(self.name_label)
+        self.labels.append(self.name_label)
 
-        # self.info_button = tk.Button(
-        #     self,
-        #     bitmap="info"
-        # )
-        # self.info_button.grid(
-        #     row=1, 
-        #     column=6, 
-        #     sticky=tk.E
-        # )
-        # self.widgets.append(self.info_button)
+        current_row = 1
 
-        for widget in self.widgets:
-            widget.grid_configure(padx=5, pady=5)
+        for param in self.params:
+
+            # Unpack expected dict values
+            name = param["name"]
+            min = param["min"]
+            max = param["max"]
+
+            # First label + entry: start value
+            label1text = f"{name} start"
+            self.labels.append(tk.Label(
+                self,
+                text=label1text
+            ))
+            self.labels[-1].grid(row=current_row, column=0)
+        
+            self.vars.append(tk.StringVar())
+            self.entries.append(tk.Entry(
+                self,
+                textvariable=self.vars[-1],
+                width=4
+            ))
+            self.entries[-1].grid(row=current_row, column=1)
+            self.vars[-1].set(min)
+
+            # Second label + entry: end value
+            label2text = f"{name} end"
+            self.labels.append(tk.Label(
+                self,
+                text=label2text
+            ))
+            self.labels[-1].grid(row=current_row, column=2)
+            
+            self.vars.append(tk.StringVar())
+            self.entries.append(tk.Entry(
+                self,
+                textvariable=self.vars[-1],
+                width=4
+            ))
+            self.entries[-1].grid(row=current_row, column=3)
+            self.vars[-1].set(max)
+
+            current_row += 1
+
+        # Set padding
+        for label in self.labels:
+            label.grid_configure(padx=5, pady=5)
+        for entry in self.entries:
+            entry.grid_configure(padx=5, pady=5)
 
     def refresh_colors(self):
-        for widget in self.widgets:
-            widget.configure(
+        for label in self.labels:
+            label.configure(
                 bg=colors["hl1"],
                 fg=colors["bg1"],
                 highlightbackground=colors["hl1"]
@@ -69,3 +117,25 @@ class Effect(tk.Frame):
                 bg=colors["bg2"],
                 fg=colors["hl2"]
             )
+
+
+
+    def get_effect_params(self):
+        output = []
+
+        for i in range(len(self.params)):
+            # Iterate through self.vars array 2 at a time
+            # for the start and end vals
+            var_idx_1 = i * 2
+            var_idx_2 = var_idx_1 + 1
+
+            new_param = {
+                "name": self.params[i]["name"],
+                "start_val": self.vars[var_idx_1].get(),
+                "end_val": self.vars[var_idx_2].get()
+            }
+
+            output.append(new_param)
+
+        print(output)
+        return output
--- effect_amplify.py
+++ effect_amplify.py
@@ -5,43 +5,12 @@ from effect import Effect
 class Amplify(Effect):
     def __init__(self, master):
 
-        Effect.__init__(self, master, "Amplify")
-        self.master = master
+        params = [
+            {
+                "name": "Ratio",
+                "min": 0.01,
+                "max": 0.99
+            }
+        ]
 
-        self.create_widgets()
-        super().refresh_colors()
-
-    def create_widgets(self):
-        
-        self.start_val_var = tk.StringVar()
-        self.start_val = tk.Entry(
-            self,
-            textvariable=self.start_val_var,
-            width=2
-        )
-        self.start_val.grid(row=1, column=0, padx=5, pady=5)
-        self.start_val_var.set(0.1)
-        self.entries.append(self.start_val)
-        
-        self.end_val_var = tk.StringVar()
-        self.end_val = tk.Entry(
-            self,
-            textvariable=self.end_val_var,
-            width=2
-        )
-        self.end_val.grid(row=1, column=1, padx=5, pady=5)
-        self.end_val_var.set(0.1)
-        self.entries.append(self.end_val)
-
-        super().create_widgets()
-
-
-    def get_effect_params(self):
-        param = {
-            "name": "Ratio",
-            "start_val": self.start_val_var.get(),
-            "end_val": self.end_val_var.get()
-        }
-        self.params.append(param)
-
-        return self.params
+        Effect.__init__(self, master, "Amplify", params)
