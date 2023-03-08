d605cd78fc2fb814e4b2a2a7d860d2ace3d7b184
Jeff Holland <holland.musictech@gmail.com>
2023-03-08 12:45:08 -0500

    saving presets - last filename saved, also can press return key to save

--- preset_save.py
+++ preset_save.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-from write import write_json
+from write import *
 
 class SavePreset(tk.Frame):
     def __init__(self, master):
@@ -18,6 +18,12 @@ class SavePreset(tk.Frame):
         )
         self.filename_entry.grid(row=0, column=0, padx=5, pady=5, columnspan=2)
         self.filename_entry.focus_set()
+        self.filename_entry.bind("<KeyPress>", self.filename_entry_keypress)
+
+        last = get_last_filename()
+        if last != None:
+            self.filename_entry.insert(0, last)
+            self.filename_entry.selection_range(0, tk.END)
 
         self.cancel_button = tk.Button(
             self.window,
@@ -36,6 +42,8 @@ class SavePreset(tk.Frame):
 
     def save(self):
         name = self.filename_entry_var.get()
+        set_last_filename(name)
+
         if name[-5:] != ".json":
             name = name + ".json"
 
@@ -48,3 +56,8 @@ class SavePreset(tk.Frame):
 
     def cancel(self):
         self.window.destroy()
+
+
+    def filename_entry_keypress(self, event):
+        if event.keysym == "Return":
+            self.save()
--- write.py
+++ write.py
@@ -1,9 +1,18 @@
 import json
 from tkinter import messagebox
 
-def write_json(filepath, data):
+# store last used filepath
+last_filename = None
 
-    print(data)
+def set_last_filename(filename):
+    global last_filename
+    last_filename = filename
+
+def get_last_filename():
+    global last_filename
+    return last_filename
+
+def write_json(filepath, data):
     try:
         with open(filepath, "w") as f:
             json.dump(data, f)
